99b9ff92e108438c32aecb1ec8565689e8191825
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2019-05-17 00:08:18 +0200

    Add simple empty project
    A live-reload based on an excellent `bellwoods` js13k entry by
    @mattdesl.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const helloWorld = () => {
+  console.log(`👼 exodus-js
+    Created by Andrzej Lichnerowicz (http://twitter.com/unjello/)
+    Audio by Jakub Argasiński (http://twitter.com/argasek)`);
+};
+
+const createApp = () => {
+  helloWorld();
+};
+
+createApp();
